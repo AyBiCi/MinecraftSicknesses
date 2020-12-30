9667010649e830d148b1842eabcb131c7b42869b
rycerz125 <jeste.oszuste.op.pl@gmail.com>
2020-12-30 11:24:59 +0100

    added maven dependency - bukkit

--- src/main/java/SicknessesPlugin.java
+++ src/main/java/SicknessesPlugin.java
@@ -1,3 +1,8 @@
-public class SicnessesPlugin {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class SicknessesPlugin extends JavaPlugin {
+public void onEnable(){
+    Bukkit.broadcastMessage("marec");
+}
 }
